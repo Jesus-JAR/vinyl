6938fa3e7f2ff463a2de26ca914cacc3f1e5229b
Jesus <jesusarraras@gmail.com>
2023-03-17 14:07:15 +0100

    primer commit

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+#use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class VinylController //extends AbstractController
+{
+    #[Route('/')]
+    public function  index(): Response
+    {
+        return new Response("Title: PB and Jams");
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if($slug){
+            $title = u(str_replace("-", " ", $slug))->title(true);
+        }
+        else{
+            $title = "No selecciono ningun tipo de música.";
+        }
+        return new Response('Title: ' . $title);
+    }
+}
